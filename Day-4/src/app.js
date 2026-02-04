f6703201ea11e74a2af3fea88fb238d7515c3904
Manas kuiri <manaskuiri28@gmail.com>
2026-02-04 20:12:16 +0530

    new

--- Day-4/src/app.js
+++ Day-4/src/app.js
@@ -3,23 +3,23 @@ const express = require("express")
 const app = express()
 app.use(express.json())
 
-const notes =[]
+const notes = []
 
-app.post("/notes",(req,res)=>{
-     notes.push(req.body);
-     res.send("Note Created");
+app.post("/notes", (req, res) => {
+      notes.push(req.body);
+      res.send("Note Created");
 })
 
-app.get("/notes",(req,res)=>{
+app.get("/notes", (req, res) => {
       res.send(notes);
 })
 
-app.delete("/notes/:index",(req,res)=>{
+app.delete("/notes/:index", (req, res) => {
       delete notes[req.params.index]
       res.send("Note Deleted Successfully")
 })
 
-app.patch("/notes/:index",(req,res)=>{
+app.patch("/notes/:index", (req, res) => {
       notes[req.params.index].description = req.body.description
       res.send("Note updated successfully")
 })
